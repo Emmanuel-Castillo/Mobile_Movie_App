3b9602781aa149996a05020ca1f146b86ea4a5ea
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-10-30 07:39:52 -0500

    Refactored CI/CD workflow file

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,99 @@
+import {ActivityIndicator, FlatList, Image, Text, View} from "react-native";
+import {images} from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
+import {useRouter} from "expo-router";
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
+import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import {useEffect, useState} from "react";
+import {updateSearchCount} from "@/services/appwrite";
+
+const Search = () => {
+    const router = useRouter();
+    const [searchQuery, setSearchQuery] = useState("");
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError,
+        refetch: loadMovies,
+        reset
+    } = useFetch(() => fetchMovies({query: searchQuery}), false)
+
+    useEffect(() => {
+
+        // Debouncing api call
+        const timeoutId = setTimeout(async () => {
+            if (searchQuery.trim()) {
+                await loadMovies();
+            } else {
+                reset()
+            }
+        }, 500)
+
+        return () => clearTimeout(timeoutId);
+    }, [searchQuery]);
+
+    useEffect(() => {
+        if (movies?.length > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0])
+        }
+    }, [movies]);
+
+    return (
+        <View className={"flex-1 bg-primary"}>
+            <Image source={images.bg} className={"flex-1 absolute w-full z-0"} resizeMode={"cover"}/>
+
+            <FlatList data={movies} renderItem={({item}) =>
+                <MovieCard {...item}/>
+            }
+                      keyExtractor={(item) => item.id.toString()}
+                      numColumns={3}
+                      className={"px-5"}
+                      columnWrapperStyle={{justifyContent: 'center', gap: 16, marginVertical: 16}}
+                      contentContainerStyle={{paddingBottom: 100}}
+                      ListHeaderComponent={
+                          <>
+                              <View className={"w-full flex-row justify-center mt-20 items-center"}>
+                                  <Image source={icons.logo} className={"w-12 h-10"}/>
+                              </View>
+                              <View className={"my-5"}>
+                                  <SearchBar
+                                      placeholder={"Search movies..."}
+                                      value={searchQuery}
+                                      onChangeText={(text) => setSearchQuery(text)}
+                                      onPress={() => {
+                                      }}/>
+                              </View>
+
+                              {moviesLoading && (
+                                  <ActivityIndicator size={"large"} color={"#0000ff"}/>
+                              )}
+                              {moviesError && (
+                                  <Text className={"text-red-500 px-5 my-3"}>
+                                      Error: {moviesError.message}
+                                  </Text>
+                              )}
+                              {!moviesLoading && !moviesError && searchQuery.trim() && movies?.length > 0 &&
+                                  <Text className={"text-xl text-white font-bold"}>
+                                      Search Results for {' '}
+                                      <Text className={"text-accent"}>{searchQuery}</Text>
+                                  </Text>}
+                          </>
+                      }
+                      ListEmptyComponent={
+                          !moviesLoading && !moviesError ? (
+                              <View className={"mt-10 px-5"}>
+                                  <Text className={"text-center text-gray-500"}>
+                                      {searchQuery.trim() ? 'No movies found' : 'Search for a movie'}
+                                  </Text>
+                              </View>
+                          ) : null
+                      }
+            />
+        </View>
+    )
+
+}
+
+export default Search;
